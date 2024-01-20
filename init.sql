f7a2196d10a38896827f680eab702671d8571684
kagan.meric <kagan.meric@enuygun.com>
2024-01-20 20:56:01 +0300

    update

--- init.sql
+++ init.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS money_honey;
 USE money_honey;
 
 CREATE TABLE IF NOT EXISTS users (
-                                     id INT AUTO_INCREMENT PRIMARY KEY,
-                                     username VARCHAR(50) NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
     hash_password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone VARCHAR(20),
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS users (
     );
 
 CREATE TABLE IF NOT EXISTS accounting (
-                                          id INT AUTO_INCREMENT PRIMARY KEY,
-                                          user_id INT NOT NULL,
-                                          amount DECIMAL(10, 2) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
     currency VARCHAR(10) NOT NULL,
     description VARCHAR(255),
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
